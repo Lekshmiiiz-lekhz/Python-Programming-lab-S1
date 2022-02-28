ee0c515fb756cd873748ad487aed31bc8eaa017f
Lekshmi Sarasan <98203541+Lekshmiiiz-lekhz@users.noreply.github.com>
2022-02-28 08:48:53 -0800

    Add files via upload

---
+++ Colorlist.py
@@ -0,0 +1,7 @@
+color_list_1 = set(["White", "Black", "Red"])
+color_list_2 = set(["Red", "Green"])
+print("Original set elements:")
+print("\nColor List 1:", color_list_1)
+print("\nColor List 2:", color_list_2)
+print("\nAll colors in Color list 1 but not in Color list 2:")
+print(color_list_1.difference(color_list_2))
