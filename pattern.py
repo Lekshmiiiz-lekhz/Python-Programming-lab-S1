45afb489c4e1c0be809e6a4537948fbfbe1cddfe
Lekshmi Sarasan <98203541+Lekshmiiiz-lekhz@users.noreply.github.com>
2022-03-04 19:21:39 -0800

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,8 @@
+rows = int(input("enter the size of the pattern : "))
+for j in range(1, rows+1):
+    print("* " * j)
+for i in range(rows + 1, 0, -1):
+    for j in range(0, i - 1):
+        print("*", end=' ')
+    print(" ")
+© 2022 GitHub, Inc.
